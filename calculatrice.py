3592625f5310da8aac7f82dfd6bf4cd09867c99f
j.melard@outlook.com <j.melard@outlook.com>
2021-05-06 19:43:26 +0200

    Ajout de la gestion de l'historique dans l'application

--- calculatrice.py
+++ calculatrice.py
@@ -22,11 +22,11 @@ class GetMessage(object):
             on_message_callback=self.on_response,
             auto_ack=True)
 
-    def on_response(self, channel, method, props, body):
-        if self.corr_id == props.correlation_id:
+    def on_response(self, _channel, _method, _properties, body):
+        if self.corr_id == _properties.correlation_id:
             self.response = body
 
-    def call(self, numbers_additions_json):
+    def call(self, numbers_additions_json, _properties):
         self.response = None
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(
@@ -35,6 +35,7 @@ class GetMessage(object):
             properties=pika.BasicProperties(
                 reply_to=self.callback_queue,
                 correlation_id=self.corr_id,
+                content_type=_properties
             ),
             body=numbers_additions_json)
         while self.response is None:
@@ -44,11 +45,12 @@ class GetMessage(object):
 @app.route('/')
 def index():
 
-    return "Dernier resultat avec versions:"
+
+    return "result"
 
 @app.route('/add/<int:add1>/<int:add2>')
 def add(add1,add2):
-    SayHello = GetMessage()
+    DoMath = GetMessage()
 
         
     data_to_convert={"term1": add1, "term2": add2}
@@ -56,11 +58,24 @@ def add(add1,add2):
     terms_additions_json_to_send = json.dumps(data_to_convert)
 
 
-    result = SayHello.call(terms_additions_json_to_send)
+    result = DoMath.call(terms_additions_json_to_send,"do_addition")
 
     return result
 
- 
+
+@app.route('/history/')
+def history():
+    DoMath = GetMessage()
+
+        
+    data_to_convert={"term1": "2", "term2": "3"}
+
+    terms_additions_json_to_send = json.dumps(data_to_convert)
+
+
+    result = DoMath.call(terms_additions_json_to_send,"get_history")
+
+    return result
 
 
 if __name__ == '__main__':
