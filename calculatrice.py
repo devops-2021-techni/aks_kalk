265acc8654597364d74c3f57155a428a5ba80566
j.melard@outlook.com <j.melard@outlook.com>
2021-05-06 11:04:55 +0200

    passage aux microservices

--- calculatrice.py
+++ calculatrice.py
@@ -1,26 +1,69 @@
-from flask import Flask, render_template, request
+from flask import Flask
+import pika, uuid, json
+
 
 app = Flask(__name__)
-app.config.from_object(__name__)
+
+def addition(Number1, Number2):
+    return Number1 + Number2
+
+class GetMessage(object):
+
+    def __init__(self):
+        self.params = pika.URLParameters('amqps://ihlzrxjn:zidQ4Z8wUFList1uiLw6IA19vWT4km8U@rat.rmq2.cloudamqp.com/ihlzrxjn')
+
+        self.connection = pika.BlockingConnection(self.params)
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
+
+    def on_response(self, channel, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, numbers_additions_json):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange='',
+            routing_key='rpc_queue',
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id,
+            ),
+            body=numbers_additions_json)
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response
 
 @app.route('/')
-def welcome():
-    return render_template('form.html')
+def index():
 
-@app.route('/result', methods=['GET', 'POST'])
-def result():
-    entry1 = request.form.get("entry1", type=int)
-    entry2 = request.form.get("entry2", type=int)
+    return "Dernier resultat : "
 
-    if (entry1 == None or entry2 == None) :
-        return render_template('form.html')
+@app.route('/hello/<prenom>/<nom>')
+def hello(prenom,nom):
+    SayHello = GetMessage()
 
-    result = addition(entry1, entry2)
+        
+    data_to_convert={"firstname": prenom, "surname": nom}
 
-    return render_template('result.html',  htmlResult=result, htmlEntry1=entry1, htmlEntry2=entry2)
+    terms_additions_json_to_send = json.dumps(data_to_convert)
 
-def addition(nombre1, nombre2):
-    return nombre1 + nombre2
+
+    result = SayHello.call(terms_additions_json_to_send)
+
+    return result
+
+
+    
 
 
 if __name__ == '__main__':
