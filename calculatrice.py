53228f9f40365b0c907b1be5c7c312310f6d8a9d
devops-2021-techni <83694807+devops-2021-techni@users.noreply.github.com>
2021-05-06 14:13:39 +0200

    Update calculatrice.py

--- calculatrice.py
+++ calculatrice.py
@@ -46,7 +46,7 @@ class GetMessage(object):
 @app.route('/')
 def index():
 
-    return "Dernier resultat : test update "
+    return "Dernier resultat : test update bis "
 
 @app.route('/hello/<prenom>/<nom>')
 def hello(prenom,nom):
