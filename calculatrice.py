4283c218658bb7f34fd67bec4405d40735225cf3
devops-2021-techni <83694807+devops-2021-techni@users.noreply.github.com>
2021-05-06 14:36:55 +0200

    Test CD AKS 2

--- calculatrice.py
+++ calculatrice.py
@@ -46,7 +46,7 @@ class GetMessage(object):
 @app.route('/')
 def index():
 
-    return "Dernier resultat : test update bis 2"
+    return "Dernier resultat : test update bis 3"
 
 @app.route('/hello/<prenom>/<nom>')
 def hello(prenom,nom):
